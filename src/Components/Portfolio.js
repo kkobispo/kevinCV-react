1c84e8160a4d120311f3054758b22650a1014ac7
Kevin Obispo <kevkarl.obispo@gmail.com>
2021-12-29 21:05:40 +0800

    Changing image sizes and adding metaverse

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,6 @@ const Portfolio = ({ data }) => {
       <div className="row">
         <div className="twelve columns collapsed">
           <h1>Check Out Some of My Works.</h1>
-
           <div
             id="portfolio-wrapper"
             className="bgrid-halves s-bgrid-halves cf"
